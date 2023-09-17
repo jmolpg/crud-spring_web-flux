51e902612049ba89f499b9ba85183f38b7fa798a
Jessica Martins <108154142+jmolpg@users.noreply.github.com>
2023-09-16 21:36:41 -0300

    Add files via upload

---
+++ JavaSwebflux/src/main/java/javasw/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package javasw.repository;
+import javasw.Model.ClientModel;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Repository
+public interface UserRepository extends ReactiveMongoRepository<ClientModel, Long> {
+    Flux<ClientModel> findClientByName(String name);
+    Mono<ClientModel> deleteById(String id);
+    Mono<ClientModel> findById(String id);
+}
